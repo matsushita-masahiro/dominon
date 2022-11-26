32b4625e7975683bce2fe45cc618823fbee81592
松下昌弘 <matsushita.samurai@gmail.com>
2022-11-26 11:54:46 +0000

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,41 +24,6 @@ user.each_with_index do |user,index|
     User.create(id: index+1, name: user, email: "#{user}@gmail.com", password: "111111", nickname: "nickname-#{user}")
 end
 
-4.times do |i|
-    Match.create(id: (i+1), name: "試合#{i+1}", place: "神泉")
-end
-
-Entry.create(match_id: 1, user_id: 1)
-Entry.create(match_id: 1, user_id: 2)
-Entry.create(match_id: 1, user_id: 3)
-
-
-Entry.create(match_id: 2, user_id: 1)
-Entry.create(match_id: 2, user_id: 2)
-Entry.create(match_id: 2, user_id: 4)
-
-Entry.create(match_id: 3, user_id: 1)
-Entry.create(match_id: 3, user_id: 2)
-Entry.create(match_id: 3, user_id: 5)
-
-Entry.create(match_id: 4, user_id: 1)
-Entry.create(match_id: 4, user_id: 2)
-Entry.create(match_id: 4, user_id: 6)
-Entry.create(match_id: 4, user_id: 7)
-
-id = 1
-Match.all.each_with_index do |m,i|
-    2.times do |k|
-        x = (i+k)%4
-        MatchInning.create(id: id, match_id: m.id, version_used: version[x], inning_number: (k+1))
-        id = id + 1
-    end
-end
-
-
-[1,2,3].each do |user|
-  MatchResult.create(match_inning_id: MatchInning.find(1).id, user_id: user, point: user*10)
-end
 
 
 ex_version_omit = [["基本", "基"],["陰謀","陰"],["収穫祭・錬金術","収錬"],["海辺","海"],["冒険","冒"],["帝国","帝"],
