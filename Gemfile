698da8485373162b318e396953ef825ebdd342c2
松下昌弘 <matsushita.samurai@gmail.com>
2022-11-28 17:28:01 +0000

    2022/11/28

--- Gemfile
+++ Gemfile
@@ -66,3 +66,7 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'devise'
 gem 'pg', group: :production
+
+gem 'dotenv-rails'
+
+gem 'roadie-rails', '~> 3.0'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_26_153403) do
+ActiveRecord::Schema.define(version: 2022_11_27_161414) do
 
   create_table "entries", force: :cascade do |t|
     t.integer "match_id", null: false
@@ -78,6 +78,10 @@ ActiveRecord::Schema.define(version: 2022_11_26_153403) do
     t.date "birthday"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
