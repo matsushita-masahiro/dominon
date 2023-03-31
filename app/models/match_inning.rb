23045055dd430df707f4ec238ee24a893b93a1f2
松下昌弘 <matsushita.samurai@gmail.com>
2023-03-31 14:55:44 +0000

    user.rb has_many match_innings

--- app/models/match_inning.rb
+++ app/models/match_inning.rb
@@ -11,6 +11,7 @@ class MatchInning < ApplicationRecord
     end
     
     
+    
 
     
     
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,12 @@ class User < ApplicationRecord
          
   has_many :entries, dependent: :destroy
   has_one :admin
+  has_many :match_results
   
+  # あるuserが参加した全game
+  # def participate_all_games
+  #   self.match_innings
+  # end
   
   # def played_matches
   #   self.entries
