52bd89af4f0eda9a916d72e5e5c93c26acf87bad
松下昌弘 <matsushita.samurai@gmail.com>
2023-04-01 19:03:31 +0000

    mypage game won number

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,12 +22,23 @@ class User < ApplicationRecord
     hash = Hash.new { |h,k| h[k] = {} }
      # このuserが参加した全gameのid配列
     self.participate_all_games.pluck(:match_inning_id).each do |match_inning_id|
-      hash = {match_inning_id => MatchResult.where(match_inning_id: match_inning_id).pluck(:user_id, :point).to_h}
-      hash[match_inning_id] = hash[match_inning_id].sort_by { |_, v| -v }.to_h  # 例 {272 => {9=>75, 6=>66, 7=>58}} ０番目に1位
+      game_result = MatchResult.where(match_inning_id: match_inning_id).pluck(:user_id, :point).to_h
+      hash[match_inning_id] = game_result.sort_by { |_, v| -v }.to_h  # 例 {272 => {9=>75, 6=>66, 7=>58}} ０番目に1位
     end
-    
-    
+
     return hash
+  end
+  
+  def first_place_of_games
+    
+    array = []
+    participate_all_games_result_hash_sorted = self.participate_all_games_result_hash_sorted
+    participate_all_games_result_hash_sorted.each{|inning, result|
+      if result.keys[0] == self.id
+        array.push(MatchInning.find(inning.to_i))
+      end
+    }
+    return array
   end
   
   # hash = {}
