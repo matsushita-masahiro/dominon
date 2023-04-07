d0ec299eb4bc112be932aefab6bcdb5c8d689993
松下昌弘 <matsushita.samurai@gmail.com>
2023-04-07 21:22:29 +0000

    users raiting sorted

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,11 +7,13 @@ class UsersController < ApplicationController
     User.all.each do |user|
       @users_hash[user.id] = user.rating
     end
+
+    # rating順にsortしたhashからuser_idを取得して@users配列にpush
+    @users_hash.sort_by { |_, v| v }.reverse.to_h.each do |user_id, rating|
+      @users << User.find(user_id)
+    end
     
-    @users_hash.sort_by { |_, v| v }.reverse.to_h
-    
-    @users = User.where(id: @users_hash.keys)
-    # logger.debug("~~~~~~~~ @users = #{@users}")
+    # logger.debug("~~~~~~~~ @users = #{@user}")
     
   end
 
